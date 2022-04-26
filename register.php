8b03a2e006c80b5aac5ff7c271e50fae592d15f8
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-04-26 00:27:23 -0300

    Register form update

--- register.php
+++ register.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Register</title>
+	<title>Registrar</title>
 	<link rel="stylesheet" href="css/component.css">
 	<link rel="shortcut icon" href="img/favicon.ico"/>
 	<!--Font awesome cdn link-->
@@ -12,9 +12,22 @@
 </head>
 <body>
 
+<!-- ======= Register Form/Formulário de Registro ======= -->
+<section class="form-container">
+	<form action="" enctype="multipart/form-data" method="POST">
+		<h3>Registrar agora</h3>
+		<input type="text" name="name" class="box" placeholder="Digite Seu Nome" required>
+		<input type="email" name="email" class="box" placeholder="Digite Seu E-mail" required>
+		<input type="password" name="pass" class="box" placeholder="Digite Sua Senha" required>
+		<input type="password" name="cpass" class="box" placeholder="Confirme Sua Senha" required>
+		<input type="file" name="image" class="box" required accept="image/jpg, image/jpeg, image/png">
+		<input type="submit" name="submit" value="Registrar Agora" class="btn">
+	</form>
+</section>
+
 	<?php
 	
-		echo "Olá, Mundo!, Sapup3 na área!!!"
+		
 	
 	?>
 	
