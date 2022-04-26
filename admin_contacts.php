e068f2f8834a19a0e91838a4ca6da876fc7cf635
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-04-25 22:46:02 -0300

    Create admin_contacts.php

---
+++ admin_contacts.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Admin Contatos</title>
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="shortcut icon" href="img/favicon.ico"/>
+</head>
+<body>
+
+	<?php
+	
+		echo "Olá, Mundo!, Sapup3 na área!!!"
+	
+	?>
+	
+	<script src="js/script.js"></script>
+</body>
+</html>
+
+<!--
+    Autor: Daniel Oliveira
+    Email: danieloliveira.webmaster@gmail.com
+    Manaus/Amazonas
+    25/04/2022
+-->
