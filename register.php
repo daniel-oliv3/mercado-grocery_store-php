2407172ff222f80eb427799bfa932856d17e084d
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-04-25 23:49:49 -0300

    Update

--- register.php
+++ register.php
@@ -5,8 +5,10 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Register</title>
-	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/component.css">
 	<link rel="shortcut icon" href="img/favicon.ico"/>
+	<!--Font awesome cdn link-->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 </head>
 <body>
 
