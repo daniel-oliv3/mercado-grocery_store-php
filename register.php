72fcc42f476a7fa41b486b6170e0126b8742bd8c
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-04-26 02:54:33 -0300

    Conexao com banco de dados

--- register.php
+++ register.php
@@ -1,26 +1,53 @@
 <?php
-	
+
 include 'config.php';
 
 if(isset($_POST['submit'])){
 
-	$name = $_POST['name'];
-	$name = filter_var($name, FILTER_SANITIZE_STRING);
-	$email = $_POST['email'];
-	$email = filter_var($email, FILTER_SANITIZE_STRING);
-	$pass = $_POST['pass'];
-	$pass = filter_var($pass, FILTER_SANITIZE_STRING);
-	$cpass = $_POST['cpass'];
-	$cpass = filter_var($cpass, FILTER_SANITIZE_STRING);
+   $name = $_POST['name'];
+   $name = filter_var($name, FILTER_SANITIZE_STRING);
+   $email = $_POST['email'];
+   $email = filter_var($email, FILTER_SANITIZE_STRING);
+   $pass = md5($_POST['pass']);
+   $pass = filter_var($pass, FILTER_SANITIZE_STRING);
+   $cpass = md5($_POST['cpass']);
+   $cpass = filter_var($cpass, FILTER_SANITIZE_STRING);
 
-	$image = $_FILES['image']['name'];
-	$image_size = $_FILES['image']['size'];
-	$image_tmp_name = $_FILES['image']['tmp_name'];
-	$image_folder = 'uploader_img/';
+   $image = $_FILES['image']['name'];
+   $image = filter_var($image, FILTER_SANITIZE_STRING);
+   $image_size = $_FILES['image']['size'];
+   $image_tmp_name = $_FILES['image']['tmp_name'];
+   $image_folder = 'uploaded_img/'.$image;
+
+   $select = $conn->prepare("SELECT * FROM `users` WHERE email = ?");
+   $select->execute([$email]);
+
+   if($select->rowCount() > 0){
+      $message[] = 'O e-mail do usuário já existe!';
+   }else{
+      if($pass != $cpass){
+         $message[] = 'Confirme a senha não correspondida!';
+      }else{
+         $insert = $conn->prepare("INSERT INTO `users`(name, email, password, image) VALUES(?,?,?,?)");
+         $insert->execute([$name, $email, $pass, $image]);
+
+         if($insert){
+            if($image_size > 2000000){
+               $message[] = 'image size is too large!';
+            }else{
+               move_uploaded_file($image_tmp_name, $image_folder);
+               $message[] = 'registered successfully!';
+               header('location:login.php');
+            }
+         }
+
+      }
+   }
 
 }
-	
+
 ?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -30,10 +57,30 @@ if(isset($_POST['submit'])){
 	<title>Registrar</title>
 	<link rel="stylesheet" href="css/component.css">
 	<link rel="shortcut icon" href="img/favicon.ico"/>
-	<!--Font awesome cdn link-->
+	<!-- `` Font awesome cdn link `` -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 </head>
 <body>
+
+<div class="message">
+	<span></span>
+	<i class="fas fatimes" onclick="this.parentElement.remove();"></i>
+</div>
+
+<?php
+
+if(isset($message)){
+   foreach($message as $message){
+      echo '
+      <div class="message">
+         <span>'.$message.'</span>
+         <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+      </div>
+      ';
+   }
+}
+
+?>
 
 <!-- ======= Register Form/Formulário de Registro ======= -->
 <section class="form-container">
